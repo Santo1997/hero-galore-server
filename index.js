c16d72a77ceafb5ce07e47d17924482928a8dca6
Santo1997 <hossainsanto2@gmail.com>
2023-05-19 10:18:00 +0600

    get data mongodb

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    const toyStore = client.db("toyManager").collection('toys')
+    
+
+    app.get("/toys", async(req, res)=>{
+        const result = await toyStore.find().toArray();
+        res.send(result);
+    })
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
