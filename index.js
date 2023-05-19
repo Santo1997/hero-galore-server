c1baaf960941ad5b97d1b738e000c05e0ad800a3
Santo1997 <hossainsanto2@gmail.com>
2023-05-19 20:36:17 +0600

    delete method

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/toys/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const toy = await toyStore.deleteOne(query);
+      res.send(toy);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
